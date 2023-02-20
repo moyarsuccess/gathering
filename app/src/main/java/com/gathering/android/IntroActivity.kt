aae507d43e41515eb29cb1e19e447660dfc0f21e
ida oskooei <idaoskooei@gmail.com>
2023-02-20 13:44:55 -0500

    [#3] findViewById the viewpager

--- app/src/main/java/com/gathering/android/IntroActivity.kt
+++ app/src/main/java/com/gathering/android/IntroActivity.kt
@@ -2,10 +2,14 @@ package com.gathering.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.intro_avtivity)
+        val viewPager: ViewPager2 = findViewById(R.id.viewpager)
     }
+
 }
