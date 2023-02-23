6ca006f092a9354e136cb8b92a0b7423d812d213
Ida Oskooei <idaoskooei@Idas-MacBook-Pro.local>
2023-02-22 20:22:08 -0500

    [T3] completing adapter and introActivity classes

--- app/src/main/java/com/gathering/android/IntroActivity.kt
+++ app/src/main/java/com/gathering/android/IntroActivity.kt
@@ -21,12 +21,18 @@ class IntroActivity : AppCompatActivity() {
     }
     private fun setupViewPager2() {
         val list: MutableList<AppIntro> = ArrayList()
-        list.add("img2".toInt(),"a platform to create events, such as parties, reunions, and gatherings")
-        list.add("img1".toInt(),"Bringing you family and friends together has never been so easy")
-        list.add("img3".toInt(),"invite guests and manage the event details")
+
+            var appIntro = AppIntro (R.drawable.img2,"a platform to create events, such as parties, reunions, and gatherings")
+            var appIntro2 = AppIntro (R.drawable.img1,"Bringing you family and friends together has never been so easy")
+            var appIntro3 = AppIntro (R.drawable.img3,"invite guests and manage the event details")
+
+
+        list.add(appIntro)
+        list.add(appIntro2)
+        list.add(appIntro3)
 
         // Set adapter to viewPager.
-        binding.viewpager.adapter = ViewPagerAdapter()
+        binding.viewpager.adapter = ViewPagerAdapter(list)
     }
 
 }
--- app/src/main/java/com/gathering/android/ViewPagerAdapter.kt
+++ app/src/main/java/com/gathering/android/ViewPagerAdapter.kt
@@ -1,36 +1,33 @@
 package com.gathering.android
 
-import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.gathering.android.databinding.ItemViewPagerBinding
 
-class ViewPagerAdapter( private val list: MutableList<AppIntro>) : RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder>() {
+class ViewPagerAdapter(private val list: MutableList<AppIntro>) :
+    RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
-            val binding = ItemViewPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return ViewPagerHolder(binding)
-        }
-
-        override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
-            val image = labelList[position]
-            val color = colorList[position]
-            holder.bind(label, color)
-        }
-
-        override fun getItemCount(): Int {
-            return labelList.size
-        }
-
-        class ViewPagerHolder(private var itemHolderBinding: ItemHolderBinding) :
-            RecyclerView.ViewHolder(itemHolderBinding.root) {
-            fun bind(label: String, color: String) {
-                itemHolderBinding.label.text = label
-                itemHolderBinding.root.setBackgroundColor(Color.parseColor(color))
-            }
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
+        val binding =
+            ItemViewPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewPagerHolder(binding)
     }
 
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        val appIntro = list[position]
+        holder.bind(appIntro)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewPagerHolder(private var itemViewPagerBinding: ItemViewPagerBinding) :
+        RecyclerView.ViewHolder(itemViewPagerBinding.root) {
+        fun bind(appIntro: AppIntro) {
+            itemViewPagerBinding.imageView1.setImageResource(appIntro.imageId)
+            itemViewPagerBinding.description1.text = appIntro.description
+        }
+    }
 }
