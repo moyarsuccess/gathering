1be9b8b124ef035275fa8a627739ffe9a1c77d17
Ani <anahidmansoubi2015@gmail.com>
2023-02-28 13:32:50 -0500

    [6] Add Hilt dependencies
    add an application class
    add an application module class
    add a dependencies
    add dagger plugin and kept

---
+++ app/src/main/java/com/gathering/android/di/ApplicationModule.kt
@@ -0,0 +1,9 @@
+package com.gathering.android.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule
