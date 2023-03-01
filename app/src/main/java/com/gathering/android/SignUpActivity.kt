7d8c6337a8f17b0ca027d9879038f9292df32143
ida oskooei <idaoskooei@gmail.com>
2023-03-01 13:17:07 -0500

    [T3] create signInActivity and SignUpActivity Class

---
+++ app/src/main/java/com/gathering/android/SignUpActivity.kt
@@ -0,0 +1,4 @@
+package com.gathering.android.intro
+
+class SignUpActivity {
+}
--- app/src/main/java/com/gathering/android/intro/IntroActivity.kt
+++ app/src/main/java/com/gathering/android/intro/IntroActivity.kt
@@ -1,5 +1,6 @@
 package com.gathering.android.intro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gathering.android.R
@@ -15,6 +16,7 @@ class IntroActivity : AppCompatActivity() {
         binding = IntroActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val IntroActivityViewModel = IntroActivityViewModel()
 
         val list = listOf(
             IntroPageFragment.AppIntro(
@@ -37,13 +39,20 @@ class IntroActivity : AppCompatActivity() {
         binding.dotsIndicator.setViewPager2(binding.viewPager)
 
 
+
         binding.signinBtn.setOnClickListener {
+            val signInIntent = Intent(this, SignInActivity::class.java)
+            startActivity(signInIntent)
         }
 
         binding.signupBtn.setOnClickListener {
+            val signUpIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(signUpIntent)
         }
     }
 }
+
+
 
 
 
