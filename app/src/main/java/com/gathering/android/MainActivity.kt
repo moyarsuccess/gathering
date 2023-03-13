dc5f0da699090591b2e78434760292c2575fa6f1
Ani <anahidmansoubi2015@gmail.com>
2023-03-13 16:08:21 -0400

    [T8] Add signUp UI page
    add a logo
    add custom button
    add custom edit text
    add icons

--- app/src/main/java/com/gathering/android/MainActivity.kt
+++ app/src/main/java/com/gathering/android/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.gathering.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.gathering.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        findViewById<Button>(R.id.btn_test).setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
