627e5ec847b4c313f975af65939e35a55488c54f
anahid <30546960+Animansoubi@users.noreply.github.com>
2023-03-14 16:39:38 -0400

    Ani/feature/t8 sign up UI (#9)

--- app/src/main/java/com/gathering/android/MainActivity.kt
+++ app/src/main/java/com/gathering/android/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gathering.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gathering.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTest.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
