af0e49aedab425bd38daeec582c9bff3a5beeea4
Ani <anahidmansoubi2015@gmail.com>
2023-05-19 15:18:31 -0400

    T30 Add add-location-bottom sheet

---
+++ app/src/main/java/com/gathering/android/event/myevent/addevent/location/AddLocationViewState.kt
@@ -0,0 +1,25 @@
+package com.gathering.android.event.myevent.addevent.location
+
+import com.google.android.gms.maps.model.LatLng
+
+sealed interface AddLocationViewState {
+    class NavigateToAddEvent(val address: String) : AddLocationViewState
+
+    class ShowError(val errorMessage: String?) : AddLocationViewState
+
+    class ShowAddressList(val addressList: List<String>) : AddLocationViewState
+
+    class SetAddress(val address: String) : AddLocationViewState
+
+    object ClearMap : AddLocationViewState
+
+    class AddMarker(val latLng: LatLng) : AddLocationViewState
+
+    class MoveCamera(val latLng: LatLng) : AddLocationViewState
+
+    object HideKeyboard : AddLocationViewState
+
+    object AdjustMapUi : AddLocationViewState
+
+    object SyncMapWithCurrentDeviceLocation : AddLocationViewState
+}
