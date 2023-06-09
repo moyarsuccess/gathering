dfa6b6bfbfe802e46f72e4e0d708db5ae8e43eec
anahid <30546960+Animansoubi@users.noreply.github.com>
2023-06-09 15:25:37 -0400

    T38: Check all the fields filled (#32)
    add functionalities to add_event_view_model to check all of the fields
    have been filled unless the add event button is not visible

--- app/src/main/java/com/gathering/android/event/myevent/addevent/AddEventViewModel.kt
+++ app/src/main/java/com/gathering/android/event/myevent/addevent/AddEventViewModel.kt
@@ -15,27 +15,48 @@ class AddEventViewModel @Inject constructor(
     private val _viewState = ActiveMutableLiveData<AddEventViewState>()
     val viewState: ActiveMutableLiveData<AddEventViewState> by ::_viewState
 
+    private var isImageFilled: Boolean = false
+    private var isEventNameFilled: Boolean = false
+    private var isHostNameFilled: Boolean = false
+    private var isDescriptionFilled: Boolean = false
+    private var isDateFilled: Boolean = false
+    private var isTimeFilled: Boolean = false
+    private var isAddressFilled: Boolean = false
+    private var isAttendeeListFilled: Boolean = false
+
     private val contactList = mutableListOf<Contact>()
+
+    fun onViewCreated() {
+        _viewState.setValue(AddEventViewState.AddEventButtonVisibility(false))
+    }
+
+    fun onImageSelected(imageUrl: String?) {
+        if (imageUrl == null) return
+        _viewState.setValue(AddEventViewState.SetImage(imageUrl))
+        isImageFilled = isImageFiled(imageUrl)
+        val errorMessage = if (isEventNameFilled) null else IMAGE_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
+        checkAllFieldsReady()
+    }
 
     fun onImageButtonClicked() {
         _viewState.setValue(AddEventViewState.NavigateToAddPic)
     }
 
-    fun onLocationButtonClicked() {
-        _viewState.setValue(AddEventViewState.NavigateToAddLocation)
-
-    }
-
-    fun onInvitationButtonClicked() {
-        _viewState.setValue(AddEventViewState.NavigateToInviteFriend(contactList))
+    fun onDateButtonClicked() {
+        _viewState.setValue(AddEventViewState.OpenDatePickerDialog)
     }
 
     fun onTimeButtonClicked() {
         _viewState.setValue(AddEventViewState.OpenTimePickerDialog)
     }
 
-    fun onDateButtonClicked() {
-        _viewState.setValue(AddEventViewState.OpenDatePickerDialog)
+    fun onLocationButtonClicked() {
+        _viewState.setValue(AddEventViewState.NavigateToAddLocation)
+    }
+
+    fun onInvitationButtonClicked() {
+        _viewState.setValue(AddEventViewState.NavigateToInviteFriend(contactList))
     }
 
     fun onAddEventButtonClicked(event: Event) {
@@ -51,12 +72,46 @@ class AddEventViewModel @Inject constructor(
         }
     }
 
-    fun onAddressChanged(address: String) {
-        _viewState.setValue(AddEventViewState.SetAddress(address))
+    fun onEventNameChanged(eventName: String) {
+        isEventNameFilled = isEventNameFiled(eventName)
+        val errorMessage = if (isEventNameFilled) null else EVENT_NAME_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
+        checkAllFieldsReady()
+    }
+
+    fun onHostChanged(host: String) {
+        isHostNameFilled = isHostNameFiled(host)
+        val errorMessage = if (isEventNameFilled) null else HOST_NAME_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
         checkAllFieldsReady()
     }
 
     fun onDescriptionChanged(description: String) {
+        isDescriptionFilled = isDescriptionFiled(description)
+        val errorMessage = if (isEventNameFilled) null else DESCRIPTION_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
+        checkAllFieldsReady()
+    }
+
+    fun onDateChanged(date: String) {
+        isDateFilled = isDateFiled(date)
+        val errorMessage = if (isEventNameFilled) null else DATE_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
+        checkAllFieldsReady()
+    }
+
+    fun onTimeChanged(time: String) {
+        isTimeFilled = isTimeFiled(time)
+        val errorMessage = if (isEventNameFilled) null else TIME_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
+        checkAllFieldsReady()
+    }
+
+    fun onAddressChanged(address: String) {
+        _viewState.setValue(AddEventViewState.SetAddress(address))
+        isAddressFilled = isAddressFiled(address)
+        val errorMessage = if (isEventNameFilled) null else ADDRESS_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
         checkAllFieldsReady()
     }
 
@@ -65,21 +120,67 @@ class AddEventViewModel @Inject constructor(
         contactList.addAll(contacts)
         val attendee = contacts.joinToString(",")
         _viewState.setValue(AddEventViewState.SetAttendeeList(attendee))
+
+        isAttendeeListFilled = isAttendeesFiled(attendee)
+        val errorMessage = if (isEventNameFilled) null else ATTENDEES_NOT_FILLED_MESSAGE
+        _viewState.setValue(AddEventViewState.ShowError(errorMessage))
         checkAllFieldsReady()
     }
 
+    private fun isImageFiled(imageUrl: String): Boolean {
+        return imageUrl.isNotEmpty() && imageUrl.isNotBlank()
+    }
+
+    private fun isEventNameFiled(eventName: String): Boolean {
+        return eventName.isNotEmpty() && eventName.isNotBlank()
+    }
+
+    private fun isHostNameFiled(host: String): Boolean {
+        return host.isNotEmpty() && host.isNotBlank()
+    }
+
+    private fun isDescriptionFiled(description: String): Boolean {
+        return description.isNotEmpty() && description.isNotBlank()
+    }
+
+    private fun isDateFiled(date: String): Boolean {
+        return date.isNotEmpty() && date.isNotBlank()
+    }
+
+    private fun isTimeFiled(time: String): Boolean {
+        return time.isNotEmpty() && time.isNotBlank()
+    }
+
+    private fun isAddressFiled(address: String): Boolean {
+        return address.isNotEmpty() && address.isNotBlank()
+    }
+
+    private fun isAttendeesFiled(attendeeList: String): Boolean {
+        return attendeeList.isNotEmpty() && attendeeList.isNotBlank()
+    }
+
     private fun checkAllFieldsReady() {
-        _viewState.setValue(AddEventViewState.AddEventButtonVisibility(isAllFieldsValid()))
+        _viewState.setValue(AddEventViewState.AddEventButtonVisibility(isAllFieldsFilled()))
     }
 
-    private fun isAllFieldsValid(): Boolean {
-        //TODO We should check if all the field have been field or not
-        return true
+    private fun isAllFieldsFilled(): Boolean {
+        return isImageFilled &&
+                isEventNameFilled &&
+                isHostNameFilled &&
+                isDescriptionFilled &&
+                isDateFilled &&
+                isTimeFilled &&
+                isAddressFilled
     }
 
-    fun onImageSelected(image: String?) {
-        if (image == null) return
-        _viewState.setValue(AddEventViewState.SetImage(image))
+    companion object {
+        private const val IMAGE_NOT_FILLED_MESSAGE = "Please pick or take a picture"
+        private const val EVENT_NAME_NOT_FILLED_MESSAGE = "Please enter event name"
+        private const val HOST_NAME_NOT_FILLED_MESSAGE = "Please enter host name"
+        private const val DESCRIPTION_NOT_FILLED_MESSAGE = "Please enter description"
+        private const val DATE_NOT_FILLED_MESSAGE = "Please select a valid date "
+        private const val TIME_NOT_FILLED_MESSAGE = "Please select a valid time"
+        private const val ADDRESS_NOT_FILLED_MESSAGE = "Please select a address"
+        private const val ATTENDEES_NOT_FILLED_MESSAGE = "Please select attendees"
     }
-
 }
