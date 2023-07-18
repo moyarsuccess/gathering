39b9809f4e8f382e176f7e5a34ae603bc11aafc1
Ani <anahidmansoubi2015@gmail.com>
2023-07-18 19:01:57 -0400

    [T66] Migrate getAll event to our own API

--- app/src/main/java/com/gathering/android/event/model/repo/ApiEventRepository.kt
+++ app/src/main/java/com/gathering/android/event/model/repo/ApiEventRepository.kt
@@ -15,12 +15,40 @@ class ApiEventRepository @Inject constructor(
     private val userRepo: UserRepo,
 ) : EventRepository {
 
-    override fun getAllEvents(onEventRequestReady: (eventRequest: ResponseState<List<EventModel>>) -> Unit) {
-        onEventRequestReady(ResponseState.Success(listOf()))
+    override fun getAllEvents(onResponseReady: (eventRequest: ResponseState<List<EventModel>>) -> Unit) {
+        eventRemoteService.getEvents(
+            pageSize = 15,
+            pageNumber = 1
+        ).enqueue(object : Callback<List<EventModel>> {
+            override fun onResponse(
+                call: Call<List<EventModel>>,
+                response: Response<List<EventModel>>
+            ) {
+                if (!response.isSuccessful) {
+                    onResponseReady(
+                        ResponseState.Failure(
+                            Exception(RESPONSE_IS_NOT_SUCCESSFUL)
+                        )
+                    )
+                    return
+                }
+                val body = response.body()
+                if (body == null) {
+                    onResponseReady(ResponseState.Failure(Exception(BODY_WAS_NULL)))
+                    return
+                }
+                onResponseReady(ResponseState.Success(body))
+            }
+
+            override fun onFailure(call: Call<List<EventModel>>, t: Throwable) {
+                onResponseReady(ResponseState.Failure(t))
+            }
+
+        })
     }
 
     override fun getMyEvents(onResponseReady: (eventRequest: ResponseState<List<EventModel>>) -> Unit) {
-        val user = userRepo.getUser()
+
         eventRemoteService.getMyEvents(pageNumber = 1, pageSize = 15)
             .enqueue(object : Callback<List<EventModel>> {
 
