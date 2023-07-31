3ec6f789e2c3b76e90cbebf660dc3fa0bd2e1be3
Ani <30546960+Animansoubi@users.noreply.github.com>
2023-07-31 11:46:39 -0400

    The home bug fixed (#60)

--- app/src/main/java/com/gathering/android/event/home/EventListAdapter.kt
+++ app/src/main/java/com/gathering/android/event/home/EventListAdapter.kt
@@ -1,18 +1,20 @@
 package com.gathering.android.event.home
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.gathering.android.R
 import com.gathering.android.common.ImageLoader
 import com.gathering.android.databinding.ItemEventBinding
 import com.gathering.android.event.Event
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
 
-@ActivityScoped
+@FragmentScoped
 class EventListAdapter @Inject constructor(
     @ApplicationContext private val context: Context,
     private val imageLoader: ImageLoader,
@@ -23,10 +25,11 @@ class EventListAdapter @Inject constructor(
     private var onFavoriteImageClickListener: (event: Event) -> Unit = {}
     private val li = LayoutInflater.from(context)
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setEventItem(eventItemList: List<Event>) {
         this.eventItemList.clear()
         this.eventItemList.addAll(eventItemList)
-        notifyItemRangeInserted(0, eventItemList.size)
+        notifyDataSetChanged()
     }
 
     fun appendEventItems(eventItemList: List<Event>) {
