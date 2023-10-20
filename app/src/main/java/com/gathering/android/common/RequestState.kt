81e29f7773ceb3240b030145bb4741bf6000baf1
Ani <30546960+Animansoubi@users.noreply.github.com>
2023-10-20 16:02:39 -0400

    T140 delete request state class (#97)

--- app/src/main/java/com/gathering/android/common/RequestState.kt
+++
@@ -1,8 +0,0 @@
-package com.gathering.android.common
-
-sealed interface RequestState {
-
-    data class Success<T>(val data: T) : RequestState
-
-    data class Failure(val exception: Exception) : RequestState
-}
