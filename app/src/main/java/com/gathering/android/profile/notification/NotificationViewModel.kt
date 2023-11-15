9c8138220c778ee7b2b67af1d27d066c78cb8e49
idaoskooei <idaoskooei@gmail.com>
2023-11-15 10:31:22 -0500

    Notification UI with dummy data to show

---
+++ app/src/main/java/com/gathering/android/profile/notification/NotificationViewModel.kt
@@ -0,0 +1,8 @@
+package com.gathering.android.profile.notification
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class NotificationViewModel @Inject constructor() : ViewModel() {
+
+}
