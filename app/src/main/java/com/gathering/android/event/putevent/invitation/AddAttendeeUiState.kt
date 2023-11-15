6e75d10d40241c05cda4ebd059466f471375a617
Ani <anahidmansoubi2015@gmail.com>
2023-11-15 16:42:30 -0500

    T170 fix bug add attendee

--- app/src/main/java/com/gathering/android/event/putevent/invitation/AddAttendeeUiState.kt
+++ app/src/main/java/com/gathering/android/event/putevent/invitation/AddAttendeeUiState.kt
@@ -1,6 +1,8 @@
 package com.gathering.android.event.putevent.invitation
 
 data class AddAttendeeUiState(
+    val attendeeEmail: String? = null,
     val attendeesEmailList: List<String> = listOf(),
     val addAttendeeButtonEnable: Boolean = false,
+    val errorMessage: String? = null,
 )
