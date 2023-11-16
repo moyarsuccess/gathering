d91cd20a142f32339a7b766aef74b935d629333a
idaoskooei <idaoskooei@gmail.com>
2023-11-16 17:48:22 -0500

    [T175] got rid of everything related to notification

--- app/src/main/java/com/gathering/android/profile/notification/Notification.kt
+++
@@ -1,7 +0,0 @@
-package com.gathering.android.profile.notification
-
-data class Notification(
-    val title: String,
-    val message: String,
-    val time: String
-)
--- app/src/main/java/com/gathering/android/profile/notification/NotificationViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.gathering.android.profile.notification
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class NotificationViewModel @Inject constructor() : ViewModel() {
-
-}
