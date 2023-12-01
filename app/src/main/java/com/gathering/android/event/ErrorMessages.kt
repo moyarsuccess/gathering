8f147a1e2f055fa274467d4b09dc59b9dddbd326
Ani <anahidmansoubi2015@gmail.com>
2023-12-01 16:53:41 -0500

    T196 migrate home to coroutine part 5

---
+++ app/src/main/java/com/gathering/android/event/ErrorMessages.kt
@@ -0,0 +1,7 @@
+package com.gathering.android.event
+
+const val LIKE_EVENT_REQUEST_FAILED = "You are not able to like this event"
+const val SERVER_NOT_RESPONDING_TO_SHOW_EVENTS = "The event list is not available now :("
+const val General_ERROR = "Oops! something wrong"
+const val DELETE_EVENT_REQUEST_FAILED = "You are not able to delete this event"
+const val SERVER_NOT_RESPONDING_TO_SHOW_MY_EVENT = "The event list is not available now :("
