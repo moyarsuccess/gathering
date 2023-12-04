c0210aeda30b50e7d54781f1c52ff9f8fa8ff705
idaoskooei <idaoskooei@gmail.com>
2023-12-04 11:28:30 -0500

    modify remote service and repository

--- app/src/main/java/com/gathering/android/auth/repo/AuthRepository.kt
+++ app/src/main/java/com/gathering/android/auth/repo/AuthRepository.kt
@@ -5,8 +5,11 @@ import com.gathering.android.common.ResponseState
 
 interface AuthRepository {
     fun forgetPassword(
+        email: String, onResponseReady: (ResponseState<String>) -> Unit
+    )
+
+    suspend fun forgetPassword1(
         email: String,
-        onResponseReady: (ResponseState<String>) -> Unit
     )
 
     fun resetPassword(
@@ -16,11 +19,23 @@ interface AuthRepository {
         onResponseReady: (ResponseState<AuthorizedResponse>) -> Unit
     )
 
+    suspend fun resetPassword1(
+        token: String,
+        password: String,
+        deviceToken: String,
+    )
+
     fun signInUser(
         email: String,
         pass: String,
         deviceToken: String,
         onResponseReady: (ResponseState<AuthorizedResponse>) -> Unit
+    )
+
+    suspend fun signInUser1(
+        email: String,
+        pass: String,
+        deviceToken: String,
     )
 
     fun signUpUser(
@@ -30,10 +45,17 @@ interface AuthRepository {
         onResponseReady: (ResponseState<String>) -> Unit
     )
 
+    suspend fun signUpUser1(
+        email: String,
+        pass: String,
+        deviceToken: String,
+    )
+
     fun sendEmailVerification(email: String, onResponseReady: (ResponseState<String>) -> Unit)
+    suspend fun sendEmailVerification1(email: String)
 
     fun emailVerify(token: String, onResponseReady: (ResponseState<AuthorizedResponse>) -> Unit)
+    suspend fun emailVerify1(token: String)
 
     fun isUserVerified(): Boolean
-
 }
