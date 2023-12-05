72c479febc7910d6af7dc683bde8774ae930ce8e
idaoskooei <idaoskooei@gmail.com>
2023-12-05 11:02:11 -0500

    modify viewmodel and api repository to handle esceptions.

--- app/src/main/java/com/gathering/android/event/eventdetail/acceptrepo/ApiAttendanceStateRepository.kt
+++ app/src/main/java/com/gathering/android/event/eventdetail/acceptrepo/ApiAttendanceStateRepository.kt
@@ -1,12 +1,27 @@
 package com.gathering.android.event.eventdetail.acceptrepo
 
+import com.gathering.android.auth.AuthException
 import com.gathering.android.event.eventdetail.AcceptType
+import com.gathering.android.event.repo.EventException
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class ApiAttendanceStateRepository @Inject constructor(
     private val acceptTypeRemoteService: AcceptTypeRemoteService
 ) : AttendanceStateRepository {
     override suspend fun setEventAcceptType(eventId: Long, accept: AcceptType) {
-        acceptTypeRemoteService.setEventAcceptType(eventId = eventId, accept = accept.type)
+        try {
+            acceptTypeRemoteService.setEventAcceptType(eventId = eventId, accept = accept.type)
+        } catch (e: HttpException) {
+            val throwable = when (e.code()) {
+                CAN_NOT_REACH_SERVER -> EventException.NotAbleToCatchAttendanceResponseException
+                else -> AuthException.General(e.code())
+            }
+            throw throwable
+        }
+    }
+
+    companion object {
+        private const val CAN_NOT_REACH_SERVER = 503
     }
 }
