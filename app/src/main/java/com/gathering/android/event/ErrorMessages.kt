42acf2f2f615f7eb7473aaafb1d9f33f05520478
Ani <30546960+Animansoubi@users.noreply.github.com>
2023-12-07 11:17:56 -0500

    T199 add coroutine to edit profile part 4 (#173)

--- app/src/main/java/com/gathering/android/event/ErrorMessages.kt
+++ app/src/main/java/com/gathering/android/event/ErrorMessages.kt
@@ -2,8 +2,9 @@ package com.gathering.android.event
 
 const val LIKE_EVENT_REQUEST_FAILED = "You are not able to like this event"
 const val SERVER_NOT_RESPONDING_TO_SHOW_EVENTS = "The event list is not available now :("
-const val General_ERROR = "Oops! something wrong"
+const val GENERAL_ERROR = "Oops! something wrong"
 const val DELETE_EVENT_REQUEST_FAILED = "You are not able to delete this event"
 const val SERVER_NOT_RESPONDING_TO_SHOW_MY_EVENT = "The event list is not available now :("
 const val SERVER_NOT_RESPONDING_TO_SHOW_MY_FAVORITE_EVENT = "The liked event list is not available now :("
 const val UPDATE_PROFILE_REQUEST_FAILED = "You are not able to update profile now"
+const val FILE_NOT_FOUND_EXCEPTION = "File can not made from URL"
--- app/src/main/java/com/gathering/android/profile/repo/ProfileRepository.kt
+++ app/src/main/java/com/gathering/android/profile/repo/ProfileRepository.kt
@@ -1,8 +1,10 @@
 package com.gathering.android.profile.repo
 
+import com.gathering.android.common.UpdateProfileResponse
+
 interface ProfileRepository {
     suspend fun updateProfile(
         displayName: String?,
         photoUri: String?
-    )
+    ): UpdateProfileResponse?
 }
