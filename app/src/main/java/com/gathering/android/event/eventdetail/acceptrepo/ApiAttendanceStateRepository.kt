8857e0ea34f413046ecc47ab5303fc83ab1f4025
idaoskooei <idaoskooei@gmail.com>
2024-01-04 14:57:04 -0500

    fix minor issues in PR

--- app/src/main/java/com/gathering/android/event/eventdetail/acceptrepo/ApiAttendanceStateRepository.kt
+++ app/src/main/java/com/gathering/android/event/eventdetail/acceptrepo/ApiAttendanceStateRepository.kt
@@ -1,25 +1,29 @@
 package com.gathering.android.event.eventdetail.acceptrepo
 
 import com.gathering.android.auth.AuthException
+import com.gathering.android.common.GeneralApiResponse
 import com.gathering.android.event.eventdetail.AcceptType
 import com.gathering.android.event.repo.EventException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import javax.inject.Inject
 
 class ApiAttendanceStateRepository @Inject constructor(
     private val acceptTypeRemoteService: AcceptTypeRemoteService
 ) : AttendanceStateRepository {
-    override suspend fun setEventAcceptType(eventId: Long, accept: AcceptType) {
-        try {
-            acceptTypeRemoteService.setEventAcceptType(eventId = eventId, accept = accept.type)
-        } catch (e: HttpException) {
-            val throwable = when (e.code()) {
-                CAN_NOT_REACH_SERVER -> EventException.NotAbleToCatchAttendanceResponseException
-                else -> AuthException.General(e.code())
+    override suspend fun setEventAcceptType(eventId: Long, accept: AcceptType): GeneralApiResponse =
+        withContext(Dispatchers.IO) {
+            try {
+                acceptTypeRemoteService.setEventAcceptType(eventId = eventId, accept = accept.type)
+            } catch (e: HttpException) {
+                val throwable = when (e.code()) {
+                    CAN_NOT_REACH_SERVER -> EventException.NotAbleToCatchAttendanceResponseException
+                    else -> AuthException.General(e.code())
+                }
+                throw throwable
             }
-            throw throwable
         }
-    }
 
     companion object {
         private const val CAN_NOT_REACH_SERVER = 503
