a8dbaa269f1a42d280da71322440e2f2c642d21a
Ani <30546960+Animansoubi@users.noreply.github.com>
2024-02-05 14:48:45 -0500

    T216 add test to address view model (#188)

--- app/src/main/java/com/gathering/android/event/ErrorMessages.kt
+++ app/src/main/java/com/gathering/android/event/ErrorMessages.kt
@@ -5,8 +5,12 @@ const val SERVER_NOT_RESPONDING_TO_SHOW_EVENTS = "The event list is not availabl
 const val GENERAL_ERROR = "Oops! something wrong"
 const val DELETE_EVENT_REQUEST_FAILED = "You are not able to delete this event"
 const val SERVER_NOT_RESPONDING_TO_SHOW_MY_EVENT = "The event list is not available now :("
-const val SERVER_NOT_RESPONDING_TO_SHOW_MY_FAVORITE_EVENT = "The liked event list is not available now :("
+const val SERVER_NOT_RESPONDING_TO_SHOW_MY_FAVORITE_EVENT =
+    "The liked event list is not available now :("
 const val UPDATE_PROFILE_REQUEST_FAILED = "You are not able to update profile now"
 const val FILE_NOT_FOUND_EXCEPTION = "File can not made from URL"
-const val SERVER_DID_NOT_CATCH_ATTENDANCE_RESPONSE = "we did not catch your response. try RSVPing again!"
+const val SERVER_DID_NOT_CATCH_ATTENDANCE_RESPONSE =
+    "we did not catch your response. try RSVPing again!"
+const val LOCATION_IS_NULL_OR_INVALID = "location is null or invalid"
+
 
